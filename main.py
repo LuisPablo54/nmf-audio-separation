ccdb53007e20e6354b9bb83b0b10c192395e20d7
LuisPablo54 <luisirach54@gmail.com>
2026-04-30 20:17:33 -0600

    Add main script for audio separation execution

---
+++ main.py
@@ -0,0 +1,12 @@
+### Documento principal para la ejecuón del código 
+
+# Paso 0: Instalamos los requerimientos para poder ejecutar el código
+# pip install -r requirements.txt
+
+### Paso 1: Importamos los datos de MUSAN
+import kagglehub
+
+# Download latest version
+path = kagglehub.dataset_download("nhattruongdev/musan-noise")
+
+
